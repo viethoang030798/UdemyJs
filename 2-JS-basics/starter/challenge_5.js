91e720103188f73a1d431587b17bbe0ca8956071
viethoang030798 <viethoang030798@gmail.com>
2019-03-11 21:31:16 +0200

    a

--- 2-JS-basics/starter/challenge_5.js
+++ 2-JS-basics/starter/challenge_5.js
@@ -1,22 +1,25 @@
 var bills = {
     price: [124, 48, 268, 180, 42],
-    tips: [],
     tipsCal: function(){
-      for (var i = 0; i < bills.price.length; i++){
-        switch (true) {
-          case bills.price[i] < 50:
-            bills.tips[i] = bills.price[i] * 0.2;
-            return bills.tips[i];
-          case bills.price[i] >= 50 && bills.price[i] <= 200:
-            bills.tips[i] = bills.price[i] * 0.15;
-            return bills.tips[i];
-          default:
-            bills.tips[i] = bills.price[i] * 0.1;
-            return bills.tips[i];
-        }
-
+      tips = [];
+      for (var i = 0; i < 5; i++){
+          var tip = [];
+          if (bills.price[i] < 50) {
+            tip[i] = bills.price[i] * 0.2;
+            tips.push(tip[i]);
+          }
+          else if (bills.price[i] >= 50 && bills.price[i] <= 200) {
+            tip[i] = bills.price[i] * 0.15;
+            tips.push(tip[i]);
+          } else {
+            tip[i] = bills.price[i] * 0.1;
+            tips.push(tip[i]);
+          }
       }
+      return tips;
     }
-}
+};
 
+bills.tips = bills.tipsCal();
+console.log(bills);
 console.log(bills.tips);
